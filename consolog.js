6290e8d12bbe7af1363b8882204895e9c6bb6f21
Lưu Hoàng Long <39117663+longcnniu@users.noreply.github.com>
2023-08-12 16:23:37 +0700

    Update consolog.js

--- consolog.js
+++ consolog.js
@@ -1,2 +1,9 @@
 console.log("hello");
 console.log("hello"); //2nd time
+
+console.log("hello code"); 
+console.log("hello code1"); 
+console.log("hello code2"); 
+console.log("hello code3");
+console.log("hello code4");
+console.log("hello code5"); 
